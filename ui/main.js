fc520c8c83c81de552de815b3c3e96e426e358e2
gouthamsharma <30790989+gouthamsharma@users.noreply.github.com>
2017-08-09 13:21:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick= function()
           }
       }
   };
-    request.open('GET','http://gouthamsharma.imad.hasura-app.io/',true);
+    request.open('GET','http://gouthamsharma.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
