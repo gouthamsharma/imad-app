b4107035a95b2670b72edf3bca37194cf690122a
gouthamsharma <30790989+gouthamsharma@users.noreply.github.com>
2017-08-09 13:19:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,21 @@
 console.log('Loaded!');
+var button=document.getElementById("clickbtn");
+button.onclick= function()
+{
+  var request = new XMLHttpRequest();
+  request.onreadystatechange=function()
+  {
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+          if(request.status===200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById('counterspan');
+              span.innnerHTML=counter.toString();
+          }
+      }
+  };
+    request.open('GET','http://gouthamsharma.imad.hasura-app.io/',true);
+    request.send(null);
+    
+};
