0ee03678c04110cf63a7018849416a046c993e84
gouthamsharma <30790989+gouthamsharma@users.noreply.github.com>
2017-08-23 15:09:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app=express();
 var config={
   user: 'gouthamsharma',
   database: 'gouthamsharma',
-  host: 'localhost',
+  host: 'db.imad.hasura-app.io',
   port:'5432',
   password: process.env.DB_PASSWORD
     
